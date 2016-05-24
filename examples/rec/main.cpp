bd634e4d586a176523a9a3fdf974662052244886
Lukas Lao Beyer <lukaslaobeyer@gmail.com>
2016-05-24 14:44:13 +0200

    Add bandwidth/gain/center frequency controls to freesrp-rec

--- examples/rec/main.cpp
+++ examples/rec/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <mutex>
 #include <condition_variable>
+#include <boost/lexical_cast.hpp>
 
 #include <freesrp.hpp>
 
@@ -15,12 +16,16 @@
 using namespace std;
 using namespace FreeSRP;
 
-enum optionIndex {NONE, HELP, OUTFILE, FPGA};
+enum optionIndex {NONE, HELP, OUTFILE, FPGA, CENTER_FREQ, BANDWIDTH, GAIN};
 const option::Descriptor usage[] = {
-        {NONE,        0, "",  "",       option::Arg::None,      "usage: freesrp-rec [options] -ofilename\noptions:"},
-        {HELP,        0, "h", "help",   option::Arg::None,      "  -h, --help                       Print usage and exit"},
-        {OUTFILE,     0, "o", "out",    option::Arg::Optional,  "  -ofilename, --out=filename       Output to specified file ('-o-' for stdout)"},
-        {FPGA,        0, "",  "fpga",   option::Arg::Optional,  "  --fpga=/path/to/bitstream.bin    Load the FPGA with the specified bitstream"},
+        {NONE,        0, "",  "",            option::Arg::None,      "usage: freesrp-rec [options] -ofilename\noptions:"},
+        {HELP,        0, "h", "help",        option::Arg::None,      "  -h, --help                     Print usage and exit"},
+        {OUTFILE,     0, "o", "out",         option::Arg::Optional,  "  -o[filename], --out=[filename] Output to specified file ('-o-' for stdout)"},
+        {FPGA,        0, "",  "fpga",        option::Arg::Optional,  "  --fpga=/path/to/bitstream.bin  Load the FPGA with the specified bitstream"},
+        {CENTER_FREQ, 0, "f", "freq",       option::Arg::Optional,   "  -f[freq], --freq=[freq]        Center frequency in hertz (70e6 to 6e9)"},
+        {BANDWIDTH,   0, "b", "bandwidth",  option::Arg::Optional,   "  -b[bw], --bandwidth=[bw]       Bandwidth in hertz (1e6 to 61.44e6)"},
+        {GAIN,        0, "g", "gain",        option::Arg::Optional,  "  -g[gain], --gain=[gain]        Gain in decibels (0 to 74)"},
+        {NONE,        0, "",  "",            option::Arg::None,      "\nexample: freesrp-rec -f2.42e9 -b4e6 -g30 -o-"},
         {0,0,0,0,0,0}
 };
 
@@ -110,6 +115,29 @@ int main(int argc, char *argv[])
 
     _out.reset(new ostream(of_buf));
 
+    double center_freq = 0, bandwidth = 0, gain = 0;
+
+    if(options[CENTER_FREQ] && options[BANDWIDTH] && options[GAIN] ||
+       options[CENTER_FREQ].arg && !options[BANDWIDTH].arg && options[GAIN].arg)
+    {
+        try
+        {
+            center_freq = boost::lexical_cast<double>(options[CENTER_FREQ].arg);
+            bandwidth = boost::lexical_cast<double>(options[BANDWIDTH].arg);
+            gain = boost::lexical_cast<double>(options[GAIN].arg);
+        }
+        catch(boost::bad_lexical_cast)
+        {
+            cout << "Error: Please specify valid numerical values" << endl;
+            return 1;
+        }
+    }
+    else
+    {
+        cerr << "Error: Please specify center frequency, bandwidth and gain. See 'freesrp-rec --help'." << endl;
+        return 1;
+    }
+
     string fpgaconfig_filename = "";
 
     if(options[FPGA])
@@ -160,6 +188,38 @@ int main(int argc, char *argv[])
         cout << "Connected to FreeSRP" << endl;
         cout << "Version: " << srp.version() << endl;
 
+        // Set center frequency
+        response r = srp.send_cmd(srp.make_command(SET_RX_LO_FREQ, center_freq));
+        if(r.error != CMD_OK)
+        {
+            std::cerr << "Could not set RX LO frequency, error: " << r.error << endl;
+            return 1;
+        }
+
+        // Set bandwidth and sample rate
+        r = srp.send_cmd(srp.make_command(SET_RX_RF_BANDWIDTH, bandwidth));
+        if(r.error != CMD_OK)
+        {
+            std::cerr << "Could not set RX bandwidth, error: " << r.error << endl;
+            return 1;
+        }
+
+        r = srp.send_cmd(srp.make_command(SET_RX_SAMP_FREQ, bandwidth));
+        if(r.error != CMD_OK)
+        {
+            std::cerr << "Could not set RX sample frequency, error: " << r.error << endl;
+            return 1;
+        }
+
+        // Set gain
+        r = srp.send_cmd(srp.make_command(SET_RX_RF_GAIN, gain));
+        if(r.error != CMD_OK)
+        {
+            std::cerr << "Could not set RX gain, error: " << r.error << endl;
+            return 1;
+        }
+
+        // Enable signal chain and start receiving samples
         start(srp);
 
         // Wait for Control-C
@@ -173,7 +233,10 @@ int main(int argc, char *argv[])
         unique_lock<mutex> lck(_interrupt_mut);
         _interrupt.wait(lck);
 
+        // Disable signal chain
         stop(srp);
+
+        return 0;
     }
     catch(const ConnectionError &e)
     {
@@ -191,5 +254,5 @@ int main(int argc, char *argv[])
                 "You can upload the FreeSRP firmware to it by running 'freesrp-ctl --fx3=/path/to/firmware.img'" << endl;
     }
 
-    return 0;
+    return 1;
 }
