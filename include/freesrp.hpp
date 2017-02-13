8abad27470b9938240c61dd558f3523e7c736d42
Lukas Lao Beyer <llb@mit.edu>
2017-02-13 17:30:43 -0500

    Add license info (GPLv3)

--- include/freesrp.hpp
+++ include/freesrp.hpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2017 by Lukas Lao Beyer <lukas@electronics.kitchen>
+ *
+ * This file is part of libfreesrp.
+ *
+ * libfreesrp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * libfreesrp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with libfreesrp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef LIBFREESRP_FREESRP_HPP
 #define LIBFREESRP_FREESRP_HPP
 
